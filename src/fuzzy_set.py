8c191cc0f65a6fc533f5e6a0d06cc437e055f411
Michał Kalenik <mchl.klnk@gmail.com>
2025-03-07 11:36:56 +0100

    Add Aplha-cut object with tests

---
+++ src/fuzzy_set.py
@@ -0,0 +1,85 @@
+from decimal import Decimal
+
+Numeric = int | float | Decimal
+Border = list[Numeric] | tuple[Numeric, ...] | Numeric
+
+class AlphaCut:
+    """
+    Represents an α-cut of a fuzzy set.
+
+    :param level: Alpha-cut level.
+    :param left_borders: Left borders of the alpha-cut. If more than one value is present, the fuzzy set is not convex.
+    :param right_borders: Right borders of the alpha-cut. If more than one value is present, the fuzzy set is not convex.
+    """
+
+    def __init__(self, level: float | Decimal, left_borders: Border, right_borders: Border) -> None:
+        self._level = level
+        self._same_type_check(left_borders, right_borders)
+        self._left_borders = self._borders_prep(left_borders)
+        self._right_borders = self._borders_prep(right_borders)
+        self._level_check()
+        self._borders_check()
+
+    @staticmethod
+    def _borders_prep(borders: Border) -> tuple[Numeric, ...]:
+        if isinstance(borders, tuple) and all([isinstance(i, Numeric) for i in borders]):
+            return borders
+        elif isinstance(borders, list) and all([isinstance(i, Numeric) for i in borders]):
+            return tuple(borders)
+        elif isinstance(borders, Numeric):
+            return tuple([borders])
+        else:
+            raise TypeError(f"Invalid borders type: {type(borders)}")
+
+    @staticmethod
+    def _same_type_check(left_borders: Border, right_borders: Border) -> None:
+        if isinstance(left_borders, tuple | list) and isinstance(right_borders, tuple | list):
+            expected_type = type(left_borders[0])
+            if not all([isinstance(border, expected_type) for border in left_borders]):
+                raise TypeError(f"Not all borders are {expected_type}")
+            if not all([isinstance(border, expected_type) for border in right_borders]):
+                raise TypeError(f"Not all borders are {expected_type}")
+
+        if type(left_borders) != type(right_borders):
+            raise TypeError(f"Left_borders and right_borders "
+                            f"must be the same type {type(left_borders), type(right_borders)}.")
+
+    def _level_check(self) -> None:
+        if 0 > self._level or self._level > 1:
+            raise ValueError("Alpha-cut level must be between 0 and 1.")
+
+    def _borders_check(self) -> None:
+        if len(self.left_borders) != len(self.right_borders):
+            raise ValueError("left and right borders must have same length.")
+        if not all([left < right for left, right in zip(self.left_borders, self.right_borders)]):
+            raise ValueError("Alpha-cut have to has positive length.")
+        if len(self.left_borders) != 1:
+            print(self.left_borders[1:], self.right_borders[:-1])
+            if not all([left <= right for left, right in zip(self.left_borders[1:], self.right_borders[:-1])]):
+                raise ValueError("Two parts of alpha-cut can't cover.")
+            if (not all([p < pp for p, pp in zip(self.left_borders, self.left_borders[1:])])
+                    or not all([p < pp for p, pp in zip(self.right_borders, self.right_borders[1:])])):
+                raise ValueError(f"Parts of alpha-cut have to be sorted.")
+
+    @property
+    def level(self) -> float | Decimal:
+        return self._level
+
+    @property
+    def left_borders(self) -> tuple[Numeric, ...]:
+        return self._left_borders
+
+    @property
+    def right_borders(self) -> tuple[Numeric, ...]:
+        return self._right_borders
+
+    def is_convex(self) -> bool:
+        return True if len(self.left_borders) == 1 and len(self.right_borders) == 1 else False
+
+
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_alphacut.py
@@ -0,0 +1,66 @@
+from src.fuzzy_set import AlphaCut, Numeric
+import pytest
+
+
+def test_alpha_cut() -> None:
+    assert AlphaCut(0.1, 0.0, 1.0)
+
+@pytest.mark.parametrize("a", [0,0.5,1])
+def test_get_alpha_level(a: float) -> None:
+    ac = AlphaCut(a, 0.0, 1.0)
+    assert ac.level == a
+
+@pytest.mark.parametrize("a", [0,0.5,1])
+def test_alpha_cut_correct_level(a: float) -> None:
+    assert AlphaCut(a, 0.0, 1.0)
+
+@pytest.mark.parametrize("a", [-0.1, 1.1])
+def test_alpha_cut_incorrect_level(a: float) -> None:
+    with pytest.raises(ValueError, match="Alpha-cut level must be between 0 and 1."):
+         AlphaCut(a, 0.0, 1.0)
+
+@pytest.mark.parametrize("left_borders, right_borders", [(1, 2),(2, 3),
+                                                         ((1, 3), (5, 7)),
+                                                         ([1, 3],[5, 7])
+                                                         ])
+def test_border_prep_corect(left_borders, right_borders) -> None:
+    ac = AlphaCut(0.1, left_borders, right_borders)
+    if isinstance(left_borders, Numeric):
+        assert ac.left_borders == tuple([left_borders])
+    if isinstance(right_borders, Numeric):
+        assert ac.right_borders == tuple([right_borders])
+
+@pytest.mark.parametrize("left_borders, right_borders", [(1, (2, 3)), ((1, 3), 2),
+                                                         (('a', 1), (1, 2)),
+                                                         ((1.2, 1), (2, 3)),
+                                                         ('a','b'),
+                                                         (('a','b'),('c','d')),
+                                                         ((1, 2), (1, 'c'))
+                                                         ])
+def test_border_prep_incorect(left_borders: float, right_borders: float ) -> None:
+    with pytest.raises(TypeError, match=r"(Invalid borders type:*|Not all borders are*|Left_borders and right_borders must be the same type*)"):
+        AlphaCut(0.1, left_borders, right_borders)
+
+@pytest.mark.parametrize("left_borders, right_borders", [((1, 2),(2, 3)), ((1, 3), (5, 7))])
+def test_alpha_cut_correct_borders(left_borders: float, right_borders: float ) -> None:
+    assert AlphaCut(0.1, left_borders, right_borders)
+
+@pytest.mark.parametrize("left_borders, right_borders", [((1, 3), (2, 4, 5)), ((1, 3), (2, 4))])
+def test_alpha_cut_incorrect_borders(left_borders: float, right_borders: float ) -> None:
+    with pytest.raises(ValueError, match=r"(left and right borders must have same length.|Two parts of alpha-cut can't cover.)"):
+        AlphaCut(0.1, left_borders, right_borders)
+
+@pytest.mark.parametrize("left_borders, right_borders", [((2, 3), (1, 4)), ((5, 2), (6, 3))])
+def test_alpha_cut_incorrect_cut_length(left_borders: float, right_borders: float ) -> None:
+    with pytest.raises(ValueError, match="(Parts of alpha-cut have to be sorted.|Alpha-cut have to has positive length.)"):
+        AlphaCut(0.1, left_borders, right_borders)
+
+@pytest.mark.parametrize("left_borders, right_borders", [((1,),(2,)), ((1,), (5,))])
+def test_alpha_cut_convex(left_borders: float, right_borders: float ) -> None:
+    ac = AlphaCut(0.1, left_borders, right_borders)
+    assert ac.is_convex() is True
+
+@pytest.mark.parametrize("left_borders, right_borders", [((1, 2),(2, 3)), ((1, 3), (5, 7))])
+def test_alpha_cut_convex(left_borders: float, right_borders: float ) -> None:
+    ac = AlphaCut(0.1, left_borders, right_borders)
+    assert ac.is_convex() is False
